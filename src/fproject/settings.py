9ae599a96cb92ff4adbc3981855da2306ebb070e
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-10 12:23:40 +0200

    Added logic to handle media and static files in development

--- src/fproject/settings.py
+++ src/fproject/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'fproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,3 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
