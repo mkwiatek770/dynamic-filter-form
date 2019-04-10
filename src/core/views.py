32b6f1bdff87d93ab142b3a0d2532cae8b99d6aa
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-10 12:37:49 +0200

    Added ListView to list all articles

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from core.models import Article
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = "articles"
+    template_name = "articles.html"
+
+    def get_queryset(self):
+        qs = Article.objects.all()
+        return qs
