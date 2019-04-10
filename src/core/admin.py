b0ad9267267951b79710c086328da3982f9b17c0
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-10 12:29:44 +0200

    First migration, created models: Article, Author, Category

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import Article, Author, Category
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(Author)
+admin.site.register(Category)
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    categories = models.ManyToManyField(Category)
+    published_date = models.DateField()
+    views = models.IntegerField(default=0)
+    reviewed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
