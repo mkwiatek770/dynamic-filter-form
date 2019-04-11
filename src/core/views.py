db5c92871fb4aa062c5ecf4d3af834f914d65a36
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-11 21:32:30 +0200

    Added logic in home view to retrieve and proccess GET parameters like title_contains id_exact title_or_author

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from core.models import Article
+from django.db.models import Q
+
+
+def condition_valid(param):
+    return param != "" and param is not None
 
 
 class ArticleListView(ListView):
@@ -9,5 +14,25 @@ class ArticleListView(ListView):
     template_name = "articles.html"
 
     def get_queryset(self):
+        request = self.request
+
+        title_contains = request.GET.get("title_contains")
+        id_exact = request.GET.get("id_exact")
+        title_or_author = request.GET.get("title_or_author")
+        view_count_min = request.GET.get("view_count_min")
+        view_count_max = request.GET.get("view_count_max")
+        date_min = request.GET.get("date_min")
+        date_max = request.GET.get("date_max")
+        category = request.GET.get("category")
+
         qs = Article.objects.all()
+
+        if condition_valid(title_contains):
+            qs = qs.filter(title__icontains=title_contains)
+        elif condition_valid(id_exact):
+            qs = qs.filter(id=id_exact)
+        elif condition_valid(title_or_author):
+            qs = qs.filter(Q(title__icontains=title_or_author)
+                           | Q(author__name__icontains=title_or_author))
+
         return qs
